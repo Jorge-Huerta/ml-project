0e137a0d535fe54a50f1d0763ed3cb46431608e4
Jorge <jorge.huerta@mail.udp.cl>
2018-12-27 21:41:23 -0300

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./presentationals/Home";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" Component={Home} />
+  </Switch>
+  //   <Router>
+  //     <div>
+  //       <Header />
+  //       <Route exact path="/" component={Home} />
+  //       <Route path="/Login" component={Login} />
+  //     </div>
+  //   </Router>
+  // );
+  //
+  // const Home = () => <h2>Home</h2>;
+  // const Login = () => <h2>Login</h2>;
+  //
+  // const Header = () => (
+  //   <ul>
+  //     <li>
+  //       <Link to="/">Home</Link>
+  //     </li>
+  //     <li>
+  //       <Link to="/login ">Login</Link>
+  //     </li>
+  //   </ul>
+);
 
 export default App;
